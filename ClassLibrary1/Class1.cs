fa7f1d7845c0dcf436b08618b9b21d016c351e52
Steve Crockett <scrockett@planview.com>
2020-05-20 18:29:53 -0500

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+  [Serializable]
+  public class Class1
+  {
+    public string ValueOne { get; set; }
+  }
+}
---
+++ WebApplication3/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Web.UI;
+using ClassLibrary1;
+
+namespace WebApplication3
+{
+  public partial class _Default : Page
+  {
+    public string Foo;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      Foo = WriteObject(new Class1 {ValueOne = "foo"});
+    }
+
+    
+    public static string WriteObject(Class1 testSerializations)
+    {
+      var writer = new System.Xml.Serialization.XmlSerializer(typeof(Class1));
+
+      var stringWriter = new StringWriter();
+
+      writer.Serialize(stringWriter, testSerializations);
+
+      return stringWriter.ToString();
+    }
+  }
+}
